21af9d08d2ef7e7e7da7ef8d6b351747477f2929
wchorski <wchorski@gmail.com>
2022-11-30 10:44:26 -0600

    first analytics branch

--- next.config.js
+++ next.config.js
@@ -2,7 +2,28 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://analytics.williamusic.com/:path*'
+      }
+    ]
+  },
   images: {
     remotePatterns:[
       {
